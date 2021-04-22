9e73fd8b2972ad1557d3032f5d441bafdd3a9e40
vicentgg <1342514547@qq.com>
2021-04-22 17:25:39 +0800

    test

--- 04.03/main.cpp
+++ 04.03/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 
 using namespace std;
 
@@ -13,16 +14,54 @@ struct TreeNode {
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x) : val(x), next(NULL) {} 
 };
 
 class Solution {
 public:
     vector<ListNode*> listOfDepth(TreeNode* tree) {
+        vector<ListNode*> lists;
 
+        if(tree == nullptr) {
+            return lists;
+        }
+
+        queue<TreeNode*> q;
+        q.emplace(tree);
+
+        while(!q.empty()) {
+            ListNode* head = nullptr;
+            ListNode* prev = nullptr;
+
+            int len = q.size();
+            for(int i = 0; i < len; i ++) {
+                TreeNode* currNode = q.front();
+                ListNode* newNode = new ListNode(currNode->val);
+                q.pop();
+
+                if(head == nullptr) {
+                    head = newNode;
+                } else {
+                    prev->next = newNode;
+                }
+                prev = newNode;
+
+                if(currNode->left) {
+                    q.emplace(currNode->left);
+                }
+                if(currNode->right) {
+                    q.emplace(currNode->right);
+                }
+            }
+
+            lists.emplace_back(head);
+        }
+
+        return lists;
+       
     }
 };
 
 int main(void) {
-    
+
 }
