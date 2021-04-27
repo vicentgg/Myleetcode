544a117a8d2ae2621f20cb5d61e28a5a5fc4c9d6
vicentgg <1342514547@qq.com>
2021-04-27 23:38:55 +0800

    view

--- plan6/main.cpp
+++ plan6/main.cpp
@@ -3,51 +3,7 @@
 
 using namespace std;
 
-struct Axis {
-    int x;
-    int y;
-    int z;
-};
-
-class Soltuion {
-
-public:
-    //将网络变为一个三维坐标数组
-    int length(int x, int y) {
-        vector<Axis> nums;
-
-        Axis start;
-        start.x = 0;
-        start.y = 0;
-        start.z = 0;
-        nums.push_back(start);
-
-        for(int i = 0; i < x; i ++) {
-
-        }
-
-        for(int j = 0; j < y; j ++) {
-
-        }
-    }
-
-};
-
 int main(void) {
-    int t;
-    cin >> t;
-    vector<int> xnum(t);
-    vector<int> ynum(t);
-
-    for(int i = 0; i < t; i ++) {
-        cin >> xnum[i];
-        cin >> ynum[i];
-    }
-
-    Soltuion s;
-
-    // for(int i = 0; i < t; i ++) {
-    //     cout << s.length(xnum[i], ynum[i]) << endl;
-    // }
-    return 0;
+    const int n = 6;
+    int m = const_cast<int> n
 }
